10554d568e280453d4937aeabcc618e12a1d950c
Tommaso <tommasotodescato@gmail.com>
2023-04-02 22:23:40 +0200

    Now the autouploader can be paused

--- src/client.py
+++ src/client.py
@@ -6,12 +6,21 @@ import webbrowser
 from tkinter import filedialog
 import functools
 
+
+# TODO: Should I refactor the structure of the code?
+# Maybe it would be better to use class functions and expand them for the GUI object,
+# so I'm sure that I won't have scope problems
+# Example: toggle_uploader only toggles state, and then i make a wrapper in the AppGUI class
+# that sets the button according to the state
+
+
 class App(ct.CTk):
 	def __init__(self):
 		super().__init__()
 		self.set_handle = files.Settings()
 		self.log_handle = files.Logs()
 		self.settings = self.set_handle.get()
+		self.uploader_state = True
 
 	def get_replays_dir(self):
 		folder = filedialog.askdirectory()
@@ -35,6 +44,17 @@ class App(ct.CTk):
 			    command=open_replay, hover=False)
 			replay_btn.grid(row=i+1, column=1)
 
+	def toggle_uploader(self):
+		# This function doesn't automatically start or stop the uploader
+		# It has to be done outside
+		self.uploader_state = not self.uploader_state
+		if self.uploader_state:
+			self.play_btn.configure(image=self.pause_img)
+		else:
+			self.play_btn.configure(image=self.play_img)
+		
+		self.set_handle.update("UploaderState", self.uploader_state)
+
 
 class AppGUI(App):
 	def __init__(self):
@@ -57,22 +77,29 @@ class AppGUI(App):
 		self.sidebar.grid(row=0, column=0, rowspan=5, sticky="nsew")
 		self.sidebar.grid_rowconfigure(0, weight=1)
 		self.sidebar.grid_rowconfigure((1, 2), weight=1)
-		self.sidebar.grid_rowconfigure(3, weight=2)
+		self.sidebar.grid_rowconfigure(3, weight=4)
 
 		self.logo_img = ct.CTkImage(img.open(f"{getcwd()}/img/logo.png"), size=(50, 50))
 		self.main_title = ct.CTkLabel(self.sidebar, text=" Sc2Sensei", image=self.logo_img, compound="left",
 			font=ct.CTkFont(size=29, weight="bold"))
 		self.main_title.grid(row=0, column=0, padx=30)
 
-		self.user_img = ct.CTkImage(img.open(f"{getcwd()}/img/user.png"), size=(20, 20))
+		self.user_img = ct.CTkImage(img.open(f"{getcwd()}/img/user.png"), size=(25, 25))
 		self.user_button = ct.CTkButton(self.sidebar, image=self.user_img, text="Username", fg_color="transparent",
 			command=self.get_username, hover=False, font=ct.CTkFont(size=14, weight="bold"))
-		self.user_button.grid(row=1, column=0, pady=50)
+		self.user_button.grid(row=1, column=0, pady=40)
 
-		self.dir_img = ct.CTkImage(img.open(f"{getcwd()}/img/dir.png"), size=(20, 20))
+		self.dir_img = ct.CTkImage(img.open(f"{getcwd()}/img/dir.png"), size=(25, 25))
 		self.replays_input = ct.CTkButton(self.sidebar, text="Replays", command=self.get_replays_dir,
 			fg_color="transparent", image=self.dir_img, hover=False, font=ct.CTkFont(size=14, weight="bold"))
-		self.replays_input.grid(row=2, column=0)
+		self.replays_input.grid(row=2, column=0, pady=(0,20))
+
+		self.play_img = ct.CTkImage(img.open(f"{getcwd()}/img/play.png"), size=(20,20))
+		self.pause_img = ct.CTkImage(img.open(f"{getcwd()}/img/pause.png"), size=(20,20))
+		self.play_btn = ct.CTkButton(self.sidebar, text="", command=self.toggle_uploader, fg_color="transparent",
+			image=self.pause_img, hover=False, width=40)
+		self.play_btn.grid(row=3, column=0)
+
 
 		# ----	UPPERBAR	----	#
 		self.upperbar = ct.CTkFrame(self, corner_radius=0, height=30)
@@ -82,8 +109,9 @@ class AppGUI(App):
 		self.upperbar.grid_columnconfigure(1, weight=8)
 		self.upperbar.grid_columnconfigure(2, weight=1)
 
-		self.welcome = ct.CTkLabel(self.upperbar, height=90, text=f"Welcome, {self.settings['Username']}")
-		self.welcome.grid(row=0, column=1)
+		self.welcome = ct.CTkLabel(self.upperbar, height=90, font=ct.CTkFont(size=18), 
+			text=f"Welcome, {self.settings['Username']}")
+		self.welcome.grid(row=0, column=1, padx=10)
 
 		self.refresh_img = ct.CTkImage(img.open(f"{getcwd()}/img/refresh.png"), size=(20, 20))
 		self.refresh_btn = ct.CTkButton(self.upperbar, text="", fg_color="transparent", image=self.refresh_img,
--- src/upload.py
+++ src/upload.py
@@ -13,28 +13,32 @@ class Uploader:
 			return None
 		
 		self.log_handle = files.Logs()
+		self.set_handle = files.Settings()
 		self.core.upload_all_new.restype = c.c_char_p
 		self.core.upload_last_n.restype = c.c_char_p
 		self.core.get_file_date.restype = c.c_longlong
 		self.core.get_dir_date.restype = c.c_longlong
 
 	def start_auto_uploader(self):
+		# TODO: Check if there is a better way to make the thread stoppable
 		self.run = True
 		while self.run:
-			old_date = self.core.get_file_date(c.c_char_p(self.data_path))
-			new_date = self.core.get_dir_date(c.c_char_p(self.replays_path))
+			if self.set_handle.get()["UploaderState"]:
 
-			if new_date > old_date:
-				print("Directory has been modified\n")
-				json_string = self.core.upload_all_new(c.c_longlong(old_date), c.c_char_p(self.replays_path))
-				self.core.wrt_file_date(c.c_char_p(self.data_path), c.c_longlong(new_date))
-				self.log_handle.add_replays(json_string)
-			elif new_date == old_date:
-				print("Ok")
-			else:
-				print("There was an error.")
+				old_date = self.core.get_file_date(c.c_char_p(self.data_path))
+				new_date = self.core.get_dir_date(c.c_char_p(self.replays_path))
 
-			time.sleep(10)
+				if new_date > old_date:
+					print("Directory has been modified\n")
+					json_string = self.core.upload_all_new(c.c_longlong(old_date), c.c_char_p(self.replays_path))
+					self.core.wrt_file_date(c.c_char_p(self.data_path), c.c_longlong(new_date))
+					self.log_handle.add_replays(json_string)
+				elif new_date == old_date:
+					print("Ok")
+				else:
+					print("There was an error.")
+
+			time.sleep(5)
 
 	def start_debug(self):
 		result = self.core.debug_mode()
