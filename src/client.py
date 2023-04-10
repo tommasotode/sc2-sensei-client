c302fab72817060d9ab60b36e346cff30cec02ec
Tommaso <tommasotodescato@gmail.com>
2023-04-10 17:37:59 +0200

    Cleanup and added infobar

--- src/client.py
+++ src/client.py
@@ -77,8 +77,7 @@ class AppGUI(App):
 		self.sidebar = ct.CTkFrame(self, corner_radius=0)
 		self.sidebar.grid(row=0, column=0, rowspan=5, sticky="nsew")
 		self.sidebar.grid_rowconfigure(0, weight=1)
-		self.sidebar.grid_rowconfigure((1, 2), weight=1)
-		self.sidebar.grid_rowconfigure(3, weight=4)
+		self.sidebar.grid_rowconfigure((1, 4), weight=4)
 
 		self.logo_img = ct.CTkImage(img.open(f"{getcwd()}/img/logo.png"), size=(50, 50))
 		self.main_title = ct.CTkLabel(self.sidebar, text=" Sc2Sensei", image=self.logo_img, compound="left",
@@ -88,19 +87,22 @@ class AppGUI(App):
 		self.user_img = ct.CTkImage(img.open(f"{getcwd()}/img/user.png"), size=(25, 25))
 		self.user_button = ct.CTkButton(self.sidebar, image=self.user_img, text="Username", fg_color="transparent",
 			command=self.get_username, hover=False, font=ct.CTkFont(size=14, weight="bold"))
-		self.user_button.grid(row=1, column=0, pady=40)
+		self.user_button.grid(row=1, column=0)
 
 		self.dir_img = ct.CTkImage(img.open(f"{getcwd()}/img/dir.png"), size=(25, 25))
 		self.replays_input = ct.CTkButton(self.sidebar, text="Replays", command=self.get_replays_dir,
 			fg_color="transparent", image=self.dir_img, hover=False, font=ct.CTkFont(size=14, weight="bold"))
-		self.replays_input.grid(row=2, column=0, pady=(0,20))
+		self.replays_input.grid(row=2, column=0)
+		
+		self.info_frame = ct.CTkFrame(self.sidebar, corner_radius=0, bg_color="transparent", fg_color="transparent")
+		self.info_frame.grid(row=4, column=0)
+		self.info_frame.grid_columnconfigure((0,1), weight=1)
 
-		self.play_img = ct.CTkImage(img.open(f"{getcwd()}/img/play.png"), size=(20,20))
-		self.pause_img = ct.CTkImage(img.open(f"{getcwd()}/img/pause.png"), size=(20,20))
-		self.current_state_img = self.pause_img if self.uploader_state else self.play_img
-		self.play_btn = ct.CTkButton(self.sidebar, text="", command=self.toggle_uploader, fg_color="transparent",
-			image=self.current_state_img, hover=False, width=40)
-		self.play_btn.grid(row=3, column=0)
+		self.git_img = ct.CTkImage(img.open(f"{getcwd()}/img/git.png"), size=(20, 20))
+		self.git_btn = ct.CTkButton(self.info_frame, image=self.git_img, text="", fg_color="transparent", bg_color="transparent",
+			hover=False, command=self.get_username, height=40, width=40)
+		self.git_btn.grid(row=0, column=0)
+
 
 
 		# ----	UPPERBAR	----	#
@@ -115,10 +117,19 @@ class AppGUI(App):
 			text=f"Welcome, {self.settings['Username']}")
 		self.welcome.grid(row=0, column=1, padx=10)
 
+
+
+		self.play_img = ct.CTkImage(img.open(f"{getcwd()}/img/play.png"), size=(20,20))
+		self.pause_img = ct.CTkImage(img.open(f"{getcwd()}/img/pause.png"), size=(20,20))
+		self.current_state_img = self.pause_img if self.uploader_state else self.play_img
+		self.play_btn = ct.CTkButton(self.upperbar, text="", command=self.toggle_uploader, fg_color="transparent",
+			image=self.current_state_img, hover=False, width=40)
+		self.play_btn.grid(row=0, column=2)
+
 		self.refresh_img = ct.CTkImage(img.open(f"{getcwd()}/img/refresh.png"), size=(20, 20))
 		self.refresh_btn = ct.CTkButton(self.upperbar, text="", fg_color="transparent", image=self.refresh_img,
 			hover=False, width=40, height=40, command=lambda:self.set_last_replays(self.log_handle.get_last_replays(3)))
-		self.refresh_btn.grid(row=0, column=2)
+		self.refresh_btn.grid(row=0, column=0, padx=(0, 10))
 
 		# ----	REPLAYS FRAME	----	#
 		self.set_last_replays(self.log_handle.get_last_replays(3))
