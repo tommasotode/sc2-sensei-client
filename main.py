ec03297a62974541dbd57815fba8ffbcbe584297
Tommaso <tommasotodescato@gmail.com>
2023-04-16 12:40:36 +0200

    Now the settings get refreshed

--- main.py
+++ main.py
@@ -5,16 +5,13 @@ import threading
 
 if __name__ == "__main__":
 	app = client.AppGUI()
-	settings = files.Settings().get()
-	uploader = up.Uploader(settings["ReplaysDir"])
-	logs = files.Logs()
+	uploader = up.Uploader()
 	# If the necessary files aren't found
 	if uploader == None:
 		exit()
 
 	up_thread = threading.Thread(target=uploader.start_auto_uploader)
 	up_thread.start()
-
 
 	# Must be in the main thread
 	app.mainloop()
--- src/upload.py
+++ src/upload.py
@@ -4,17 +4,18 @@ import time
 import src.files as files
 
 class Uploader:
-	def __init__(self, replays_path):
+	def __init__(self):
 		self.core = c.CDLL(f"{os.getcwd()}/bin/core.so")
-		self.set_handle = files.Settings()
-		self.replays_path = replays_path
-		self.username = self.set_handle.get()["Username"]
-		if not self.core.check_files(c.c_char_p(self.replays_path.encode())):
+		self.settings_handle = files.Settings()
+		self.logs_handle = files.Logs()
+		self.replays_path = self.settings_handle.get()["ReplaysDir"]
+		
+		if not self.core.check_files(c.c_char_p(self.replays_path.encode())) \
+													and self.replays_path != "":
 			print("Aborting")
 			return None
-		self.log_handle = files.Logs()
+		
 		self.run = False
-
 		self.core.upload_all_new.restype = c.c_char_p
 		self.core.upload_last_n.restype = c.c_char_p
 		self.core.get_dir_date.restype = c.c_longlong
@@ -22,19 +23,23 @@ class Uploader:
 	def start_auto_uploader(self):
 		self.run = True
 		while self.run:
-			settings = self.set_handle.get()
-			if settings["UploaderState"]:
+			settings = self.settings_handle.get()
+			up_state = settings["UploaderState"]
+			if up_state:
+				username = settings["Username"]
+				rep_path = settings["ReplaysDir"]
 				old_date = settings["LastModifiedDate"]
-				new_date = self.core.get_dir_date(c.c_char_p(self.replays_path.encode()))
+				new_date = self.core.get_dir_date(c.c_char_p(rep_path.encode()))
 				if new_date > old_date:
-					print("Directory has been modified\n")
+					localt = time.strftime('%H:%M:%S', time.localtime())
+					print(f"Directory has been modified at {localt}\n")
+					
 					json_string = self.core.upload_all_new(c.c_longlong(old_date), 
-						c.c_char_p(self.replays_path.encode()), c.c_char_p(self.username.encode()))
-					self.set_handle.update("LastModifiedDate", new_date)
-					self.log_handle.add_replays(json_string)
+						c.c_char_p(rep_path.encode()), c.c_char_p(username.encode()))
+					self.settings_handle.update("LastModifiedDate", new_date)
+					self.logs_handle.add_replays(json_string)
 				else:
 					print("Ok")
-
 			time.sleep(5)
 
 	def start_debug(self):
