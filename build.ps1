39e2df771006befec3a8de1ee4bfccc69f062894
Tommaso <tommasotodescato@gmail.com>
2023-08-21 14:12:19 +0200

    now build script creates release

--- build.ps1
+++ build.ps1
@@ -5,8 +5,15 @@ $binary = "C:\repos\Sc2SenseiClient\bin\core.so;."
 
 pyinstaller --noconfirm --onedir --add-data $data --add-binary $binary main.py 
 
-Write-Output "------ Build Completed ------"
-xcopy .\bin\ .\dist\main\bin\
-xcopy .\img\ .\dist\main\img\
-Write-Output "------ Images and Libraries Moved to dist\main ------"
-Write-Output "------ FINISH ------"
+$bin = ".\dist\main\bin\"
+$img = ".\dist\main\img\"
+
+xcopy .\bin\ $bin
+xcopy .\img\ $img
+
+$path = ".\dist\main\release"
+$time = Get-Date
+$release = Read-Host "[?] release version: "
+
+Set-Content -Path $path -Value $release -Force
+Add-Content -Path $path -Value $time -NoNewLine
